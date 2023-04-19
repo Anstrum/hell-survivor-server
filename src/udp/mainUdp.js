951485c356da302dc5d316e72d0688e64ca57b84
Nako <Nanaka.kumiho@gmail.com>
2023-04-19 09:03:55 +0200

    switching computer

--- src/udp/mainUdp.js
+++ src/udp/mainUdp.js
@@ -1,17 +1,32 @@
 import dgram from "dgram";
+import {isValidToken} from "../http/utils/jwt.js"
+const server = dgram.createSocket("udp4");
 
-const server = dgram.createSocket({type: "udp4"});
-server.bind(12345)
 
-server.on("listening", () => {
-    console.log("listening udp server in port 12345");
-})
+server.on('error', (err) => {
+    console.log(`Server error:\n${err.stack}`);
+    server.close();
+  });
+  
+  server.on('message', (msg, rinfo) => {
+    console.log(`Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}`);
+    console.log(`Message: ${msg.toString()}`);
+  });
+  server.bind(12345); // Replace 12345 with your desired port number
+  
+  server.on('listening', () => {
+  });
+  
 
-server.on("message", (msg, info) => {
-    console.log(msg.toString());
-    console.log(info.address);
+// server.on("message", (msg, info) => {
+//     console.log(msg.toString());
+//     console.log(info.address);
+//     if(isValidToken(msg.toString())){
+//         console.log("token valid");
+//     }
+// })
+// server.on("listening", () => {
+//     console.log("listening udp server in port 87887");
+// })
 
-    if (msg.toString() === "ping") {
-        server.send(JSON.stringify({xPos: 231, yPos: 3021}), info.port, info.address)
-    }
-})
+// server.bind(87887)
