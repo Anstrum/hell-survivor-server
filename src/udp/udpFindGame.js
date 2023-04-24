2b73071aed83673a36e24185f44deb50e9882950
Nako <Nanaka.kumiho@gmail.com>
2023-04-24 15:51:35 +0200

    test

--- src/udp/udpFindGame.js
+++ src/udp/udpFindGame.js
@@ -24,6 +24,7 @@ export function findGame(server, body, info) {
             let player = new Players();
                 player.id = body.id;
                 player.token = body.token;
+                player.info = info;
 
                 Game.players.push(player);
                 Game.CheckStart(server);
